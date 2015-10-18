fc74f4656262794475b838984844079dc0e67fbc
Andrew Cook <ariscop@gmail.com>
2015-10-18 17:11:07 +1100

    Merge the two acousticbrainz plugins

--- plugins/acousticbrainz/acousticbrainz.py
+++ plugins/acousticbrainz/acousticbrainz.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Acousticbrainz plugin for Picard
 # Copyright (C) 2015  Andrew Cook
+# Copyright (C) 2015  Sophist
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License version 2 as
@@ -16,19 +17,24 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 
-PLUGIN_NAME = u'AcousticBrainz Mood-Genre'
-PLUGIN_AUTHOR = u'Andrew Cook'
-PLUGIN_DESCRIPTION = u'''Uses AcousticBrainz for mood and genre.
-
-WARNING: Experimental plugin. All guarantees voided by use.'''
+PLUGIN_NAME = u'AcousticBrainz'
+PLUGIN_AUTHOR = u'Andrew Cook, Sophist'
+PLUGIN_DESCRIPTION = u'''Add's the following tags:
+<ul>
+<li>Key (in ID3v2.3 format)</li>
+<li>Beats Per Minute (BPM)</li>
+</ul>
+from the AcousticBrainz database.<br/><br/>
+Note: This plugin requires Picard 1.4.'''
 PLUGIN_LICENSE = "GPL-2.0"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.txt"
 PLUGIN_VERSION = "0.0"
 PLUGIN_API_VERSIONS = ["0.15"]
 
-from json import loads
+import json
 from functools import partial
-from picard import log
+from picard import config, log
+from picard.util import LockableObject
 from picard.metadata import register_track_metadata_processor
 from picard.webservice import REQUEST_DELAY
 
@@ -41,7 +47,7 @@ def result(album, metadata, data, reply, error):
     moods = []
     genres = []
     try:
-        data = loads(data)["highlevel"]
+        data = json.loads(data)["highlevel"]
         for k, v in data.items():
             if k.startswith("genre_") and not v["value"].startswith("not_"):
                 genres.append(v["value"])
@@ -68,3 +74,52 @@ def process_track(album, metadata, release, track):
     album._requests += 1
 
 register_track_metadata_processor(process_track)
+
+class AcousticBrainz_Key:
+
+    def get_data(self, album, track_metadata, trackXmlNode, releaseXmlNode):
+        recordingId = track_metadata['musicbrainz_recordingid']
+        if recordingId:
+            log.debug("%s: Add AcusticBrainz request for %s (%s)", PLUGIN_NAME, track_metadata['title'], recordingId)
+            self.album_add_request(album)
+            path = "/%s/low-level" % recordingId
+            return album.tagger.xmlws.get(
+                        ACOUSTICBRAINZ_HOST,
+                        ACOUSTICBRAINZ_PORT,
+                        path,
+                        partial(self.process_data, album, track_metadata),
+                        xml=False, priority=True, important=False)
+        return
+
+    def process_data(self, album, track_metadata, response, reply, error):
+        if error:
+            log.error("%s: Network error retrieving acousticBrainz data for recordingId %s",
+                PLUGIN_NAME, track_metadata['musicbrainz_recordingid'])
+            self.album_remove_request(album)
+            return
+        data = json.loads(response)
+        if "tonal" in data:
+            if "key_key" in data["tonal"]:
+                key = data["tonal"]["key_key"]
+                if "key_scale" in data["tonal"]:
+                    scale = data["tonal"]["key_scale"]
+                    if scale == "minor":
+                        key += "m"
+                track_metadata["key"] = key
+                log.debug("%s: Track '%s' is in key %s", PLUGIN_NAME, track_metadata["title"], key)
+        if "rhythm" in data:
+            if "bpm" in data["rhythm"]:
+                bpm = int(data["rhythm"]["bpm"] + 0.5)
+                track_metadata["bpm"] = bpm
+                log.debug("%s: Track '%s' has %s bpm", PLUGIN_NAME, track_metadata["title"], bpm)
+        self.album_remove_request(album)
+
+    def album_add_request(self, album):
+        album._requests += 1
+
+    def album_remove_request(self, album):
+        album._requests -= 1
+        if album._requests == 0:
+            album._finalize_loading(None)
+
+register_track_metadata_processor(AcousticBrainz_Key().get_data)
